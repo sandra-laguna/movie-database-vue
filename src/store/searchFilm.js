d0e0663ee6847fa7d2dc23180f3b82a164822446
sandra-laguna <idsandrax@gmail.com>
2021-10-06 00:28:22 +0200

    add modal to "searchFilms"

--- src/store/searchFilm.js
+++ src/store/searchFilm.js
@@ -11,7 +11,7 @@ export default {
   actions: {
     async fetchSearchFilm({ commit }, query) {
       const response = await axios.get(
-        "https://api.themoviedb.org/3/search/movie?api_key=a03066c5d8bc0cacef0bf9aeed31def9&language=es-ES&query=" +
+        "https://api.themoviedb.org/3/search/movie?api_key=a03066c5d8bc0cacef0bf9aeed31def9&language=en-US&query=" +
           query
       );
       commit("SET_SEARCH_FILM", response.data.results);
--- src/views/SearchFilm.vue
+++ src/views/SearchFilm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full">
-    <h1 class="text-center text-5xl font-bold py-8">Search a film</h1>
+    <h1 class="font-abel text-center text-5xl font-bold py-8">Search a film</h1>
     <div class="flex items-center justify-center">
       <div class="flex border-2 border-gray-200 rounded">
         <input
@@ -17,19 +17,57 @@
     </div>
 
     <div class="grid grid-cols-6 px-4 gap-y-6 mt-8">
-      <div v-for="film in searchFilm" :key="film.id" class="flex flex-col items-center">
+      <div v-for="(film, index) in searchFilm" :key="index" class="flex flex-col items-center">
         <p class="text-center h-16 flex items-center text-xl">{{ film.original_title }}</p>
-
         <img
           v-if="film.poster_path"
           class="w-56 h-80 rounded-md"
           :src="`${urlImg}${film.poster_path}`"
-          :alt="`{film.poster_path}`"
+          :alt="`${film.original_title}`"
+          @click="showDialog(index, film)"
         />
-        <img v-else class="w-56 h-80 rounded-md" src="@/assets/img/noimg.png" alt="no-image" />
+        <img v-else class="w-56 h-80 rounded-md" src="@/assets/img/noimg.png" alt="no-image" @click="open('simple')" />
       </div>
     </div>
   </div>
+
+  <div class="modal w-3/6 h-3/5 fixed inset-0 m-auto opacity-100 bg-white rounded-md shadow-xl p-5" v-show="modal">
+    <div
+      class="h-80 bg-cover flex items-end rounded-md"
+      :style="{
+        'background-image':
+          'linear-gradient(to bottom, rgba(11,30,57, 0.15), rgba(0, 0, 0, 0.90)), url(' +
+          urlImg +
+          contentModal.backdrop_path +
+          ')'
+      }"
+    >
+      <h1 class="text-center text-5xl p-4">{{ contentModal.original_title }}</h1>
+    </div>
+    <p v-if="contentModal.overview.length >= 10" class="h-36 text-black text-xl py-6">
+      {{ contentModal.overview.substring(0, 300) + "..." }}
+    </p>
+
+    <div class="flex justify-between">
+      <div class="flex flex-row">
+        <div class="bg-blue-600 py-1 px-4 text-2xl">{{ contentModal.vote_average }}</div>
+        <div class="text-black border border-blue-600 text-center py-1 px-4 text-xs font-extrabold ml-2">
+          {{ contentModal.vote_count }}<br />Ratings
+        </div>
+      </div>
+      <button v-on:click="modal = !modal" class="py-1 px-4 bg-red-500 text-lg rounded-lg hover:bg-red-400">
+        Close
+      </button>
+    </div>
+  </div>
+
+  <!-- Modal -->
+  <div
+    class="bgModal block fixed top-0 left-0 opacity-100 w-screen h-screen"
+    v-show="modal"
+    v-on:click="modal = !modal"
+  ></div>
+  <!-- End modal -->
 </template>
 
 <script>
@@ -38,12 +76,13 @@ export default {
   data() {
     return {
       search: "",
-      urlImg: "https://image.tmdb.org/t/p/original"
+      urlImg: "https://image.tmdb.org/t/p/original",
+      modal: false,
+      contentModal: ""
     };
   },
   computed: {
     searchFilm() {
-      console.log(this.$store.state.searchFilm);
       return this.$store.state.searchFilm.searchFilm;
     }
   },
@@ -51,6 +90,15 @@ export default {
     getSearchResults(search) {
       console.log(search);
       return this.$store.dispatch("fetchSearchFilm", search);
+    },
+    open(name) {
+      this.$store.dispatch("modals/open", name);
+    },
+    showDialog: function (num, name) {
+      this.modal = true;
+      this.contentModal = name;
+      let modal = "toggleModal " + num;
+      this[modal] = true;
     }
   }
   /*   async mounted() {
@@ -58,3 +106,31 @@ export default {
   } */
 };
 </script>
+
+<style>
+.modal {
+  padding: 12px;
+  z-index: 10001;
+  transition: 0.6s;
+}
+
+.bgModal {
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 10000;
+  transition: 0.3s;
+}
+
+.block-ellipsis {
+  display: block;
+  display: -webkit-box;
+  max-width: 100%;
+  height: 43px;
+  margin: 0 auto;
+  font-size: 14px;
+  line-height: 1;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
